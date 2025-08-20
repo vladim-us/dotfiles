b7a835f38c572f89597e998bc92ddb9ebba60571
Vlad <ustimenkovlad@gmail.com>
2025-08-20 08:40:50 +0200

    reenable supermaven

--- nvim/lua/custom/plugins/supermaven.lua
+++ nvim/lua/custom/plugins/supermaven.lua
@@ -1,16 +1,15 @@
 return {
-  'supermaven-inc/supermaven-nvim',
-  event = 'InsertEnter',
-  enabled = false,
-  cmd = {
-    'SupermavenUseFree',
-    'SupermavenUsePro',
-  },
-  opts = {
-    keymaps = {
-      accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
-    },
-    disable_inline_completion = vim.g.ai_cmp,
-    ignore_filetypes = { 'bigfile', 'snacks_input', 'snacks_notif' },
-  },
+	'supermaven-inc/supermaven-nvim',
+	event = 'InsertEnter',
+	cmd = {
+		'SupermavenUseFree',
+		'SupermavenUsePro',
+	},
+	opts = {
+		keymaps = {
+			accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
+		},
+		disable_inline_completion = vim.g.ai_cmp,
+		ignore_filetypes = { 'bigfile', 'snacks_input', 'snacks_notif' },
+	},
 }
