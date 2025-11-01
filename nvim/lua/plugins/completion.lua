f07d14537873b86e670a1ec16d150d63fcb34f5e
Vlad <ustimenkovlad@gmail.com>
2025-11-01 17:18:14 +0100

    add avante

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -67,4 +67,102 @@ return {
       }
     end,
   },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false,
+    opts = {
+      provider = 'ollama',
+      auto_suggestions_provider = 'ollama',
+      providers = {
+        ollama = {
+          model = 'gpt-oss:20b',
+          temperature = 0.7,
+          max_tokens = 2048,
+          ['local'] = true,
+        },
+      },
+      behaviour = {
+        auto_suggestions = false, -- Set to true if you want automatic suggestions
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+      },
+      mappings = {
+        --- @class AvanteConflictMappings
+        diff = {
+          ours = 'co',
+          theirs = 'ct',
+          all_theirs = 'ca',
+          both = 'cb',
+          cursor = 'cc',
+          next = ']x',
+          prev = '[x',
+        },
+        suggestion = {
+          accept = '<M-l>',
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-]>',
+        },
+        jump = {
+          next = ']]',
+          prev = '[[',
+        },
+        submit = {
+          normal = '<CR>',
+          insert = '<C-s>',
+        },
+        sidebar = {
+          apply_all = 'A',
+          apply_cursor = 'a',
+          switch_windows = '<Tab>',
+          reverse_switch_windows = '<S-Tab>',
+        },
+      },
+      hints = { position = 'right' },
+      windows = {
+        wrap = true, -- similar to vim.o.wrap
+        width = 30, -- default % based on available width
+        sidebar_header = {
+          align = 'center', -- left, center, right for title
+          rounded = true,
+        },
+        input = {
+          height = 8,
+        },
+      },
+      highlights = {
+        diff = {
+          current = 'DiffText',
+          incoming = 'DiffAdd',
+        },
+      },
+      --- @class AvanteConflictUserConfig
+      diff = {
+        autojump = true,
+        --- @type "diff0" | "diff1" | "diff2" | "diff3" | "diff4" | nil
+        list_opener = 'copen',
+      },
+    },
+    -- if you want to build from source then do make BUILD_FROM_SOURCE=true
+    build = 'make',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
 }
